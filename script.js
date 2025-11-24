687e6f63630d6587749748dae40c781914e75f9c
Gael <gael.harmand-thekal@etu.umontpellier.fr>
2025-11-24 17:48:40 +0100

    Version 1.1 hotfix

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener('DOMContentLoaded',()=>{
   let changelog=document.getElementById('changelog')
-  fetch("/changelog.txt")
+  fetch("changelog.txt")
   .then(response=>{
     return response.text()})
   .then(text=>{

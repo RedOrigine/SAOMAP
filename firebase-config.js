eb5798e8dbf2ee4f01850e20980a87ab2965978e
Gael <gael.harmand-thekal@etu.umontpellier.fr>
2025-12-10 07:46:44 +0100

    Another subVersion

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyD0N1VrLACgsJNS4VfALQwTB2RjDEVzk",
+  authDomain: "saomap-c37db.firebaseapp.com",
+  projectId: "saomap-c37db",
+  storageBucket: "saomap-c37db.firebasestorage.app",
+  messagingSenderId: "454503133414",
+  appId: "1:454503133414:web:746b5efc696e6eb6624b27"
+};

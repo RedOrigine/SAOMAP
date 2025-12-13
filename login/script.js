8c7d1ef9b78dd5e98a90f81ec0565600837e50d1
Gael <gael.harmand-thekal@etu.umontpellier.fr>
2025-12-13 17:14:00 +0100

    Version 1.7

--- login/script.js
+++ login/script.js
@@ -9,6 +9,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
     const loginForm = document.getElementById("loginForm");
     loginForm.addEventListener("submit", (e) => {
       e.preventDefault();
+      document.getElementById("erreurConnection").style.visibility="hidden"
       const email = document.getElementById("loginEmail").value;
       const password = document.getElementById("loginPassword").value;
 
@@ -16,18 +17,43 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
         .then(() => {
           window.location.href = "/SAOMAP/index.html";
         })
-        .catch(err => alert(err.message));
+        .catch(err => {
+          document.getElementById("erreurConnection").style.visibility="visible"
+          console.log(err)
+        }
+        );
     });
 
     // Reset password
-    const resetBtn = document.getElementById("resetBtn");
-    resetBtn.addEventListener("click", () => {
-      const email = document.getElementById("loginEmail").value;
-      if (!email) {
-        alert("Veuillez entrer votre email pour réinitialiser le mot de passe");
-        return;
-      }
+    const resetForm = document.getElementById("resetForm");
+    resetForm.addEventListener("submit", (e) => {
+      e.preventDefault()
+      var erreurReset=document.getElementById("erreurReset")
+      var messageReset=document.getElementById("messageReset")
+      messageReset.style.visibility="hidden"
+      messageReset.style.display="block"
+      erreurReset.style.visibility="hidden"
+        erreurReset.style.display="none"
+      const email = document.getElementById("resetEmail").value;
       sendPasswordResetEmail(auth, email)
-        .then(() => alert("Email de réinitialisation envoyé !"))
-        .catch(err => alert(err.message));
+        .then(() => document.getElementById("messageReset").style.visibility="visible")
+        .catch(err => {
+          messageReset.style.visibility="hidden"
+          messageReset.style.display="none"
+          erreurReset.style.visibility="visible"
+          erreurReset.style.display="block"
+          console.log(err)
+        });
     });
+
+document.addEventListener('click',(e)=>{
+  if(e.target.id==="resetBtn"){
+    var login=document.getElementById('loginDiv').classList.remove('active')
+    var reset=document.getElementById('resetDiv').classList.add('active')
+    
+  }
+  if(e.target.id==="loginBtn"){
+    var login=document.getElementById('loginDiv').classList.add('active')
+    var reset=document.getElementById('resetDiv').classList.remove('active')
+  }
+})
